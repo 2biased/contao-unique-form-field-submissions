c679528573b5cf141b302847743d8a45568c0478
2biased <2biased@proton.me>
2024-04-11 11:30:09 +0200

    remove unsed dev tools

--- ecs.php
+++ ecs.php
@@ -11,22 +11,10 @@ declare(strict_types=1);
  */
 
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
-use PhpCsFixer\Fixer\ControlStructure\EmptyLoopBodyFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->sets([__DIR__.'/../vendor/contao/easy-coding-standard/config/contao.php']);
-
     $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
         'header' => "This file is part of Contao Validation Bundle.\n\n@author 2biased <2biased@proton.me>\n\n@license LGPL-3.0-or-later",
     ]);
-
-    $ecsConfig->ruleWithConfiguration(EmptyLoopBodyFixer::class, ['style' => 'semicolon']);
-
-    $ecsConfig->parallel();
-    $ecsConfig->lineEnding("\n");
-
-    $parameters = $ecsConfig->parameters();
-    $parameters->set(Option::CACHE_DIRECTORY, sys_get_temp_dir().'/ecs_default_cache');
 };
